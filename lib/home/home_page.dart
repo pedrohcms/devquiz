5b7f818c6dcc70f7c265d52e800c488eeeed68c0
pedrohcms <pedrohcms2018@gmail.com>
2021-04-21 20:02:17 -0300

    Initial commit

---
+++ lib/home/home_page.dart
@@ -0,0 +1,93 @@
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/home/home_controller.dart';
+import 'package:dev_quiz/home/home_state.dart';
+import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller.state == HomeState.sucess) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: 'Fácil',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Médio',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Difícil',
+                  ),
+                  LevelButtonWidget(
+                    label: 'Perito',
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                child: GridView.count(
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  crossAxisCount: 2,
+                  children: controller.quizzes!
+                      .map((e) => QuizCardWidget(
+                            title: e.title,
+                            percentage: e.questionAnswered / e.questions.length,
+                            completed:
+                                "${e.questionAnswered}/${e.questions.length}",
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,58 @@
+import 'package:dev_quiz/core/app_gradients.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:dev_quiz/shared/models/user_model.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  final UserModel user;
+
+  AppBarWidget({required this.user})
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: user.name,
+                              style: AppTextStyles.titleBold,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(user.photoUrl),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  child: ScoreCardWidget(),
+                  alignment: Alignment(0.0, 1.0),
+                )
+              ],
+            ),
+          ),
+        );
+}
