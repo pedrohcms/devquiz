6515e2a7822101c9ee4c0dc27d3c3f8e95f44501
pedrohcms <pedrohcms2018@gmail.com>
2021-04-23 13:07:03 -0300

    Adiciona navegaçao entre telas

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,8 +1,14 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:dev_quiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
+  final List<QuestionModel> questions;
+
+  const ChallengePage({Key? key, required this.questions}) : super(key: key);
+
   @override
   _ChallengePageState createState() => _ChallengePageState();
 }
@@ -12,14 +18,50 @@ class _ChallengePageState extends State<ChallengePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: Size.fromHeight(86),
         child: SafeArea(
           top: true,
-          child: QuestionIndicatorWidget(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              QuestionIndicatorWidget(),
+            ],
+          ),
         ),
       ),
       body: QuizWidget(
-        title: "Oque o Flutter faz em sua totalidade?",
+        question: widget.questions[0],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 6, right: 20, left: 20),
+        child: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: NextButtonWidget.white(
+                  label: "Pular",
+                  onTap: () {},
+                ),
+              ),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                child: NextButtonWidget.green(
+                  label: "Confirmar",
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,40 +1,32 @@
 import 'package:dev_quiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
 class QuizWidget extends StatelessWidget {
-  final String title;
-  QuizWidget({Key? key, required this.title}) : super(key: key);
+  final QuestionModel question;
+  QuizWidget({Key? key, required this.question}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            title,
+            this.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente.',
-            isSelected: true,
-            isRight: true,
-          ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente.',
-          ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente.',
-          ),
-          AnswerWidget(
-            title:
-                'Possibilita a criação de aplicativos compilados nativamente.',
+          ...question.awnsers.map(
+            (e) => AnswerWidget(
+              title: e.title,
+              isRight: e.isRight,
+            ),
           ),
         ],
       ),
